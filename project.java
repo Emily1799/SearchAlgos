0f99f81f2a8dfce3d3e11c8f9930a840fa2b9622
unknown <emilyrb1799@gmail.com>
2018-04-24 02:15:38 -0400

    Added mergesort. Worked around problem where QS is mutating list

--- project.java
+++ project.java
@@ -2,6 +2,7 @@
 import java.util.Random;
 import java.util.ArrayList;
 import java.lang.System.*;
+import java.util.Arrays;
 
 /**
  * Write a description of class project here.
@@ -11,7 +12,7 @@ import java.lang.System.*;
  */
 public class project {
 
-    public static ArrayList<Integer> Quicksort(ArrayList<Integer> list, int pivot) {
+    public static ArrayList<Integer> QuickSort(ArrayList<Integer> list, int pivot) {
         ArrayList<Integer> greater_or_equal = new ArrayList<>();
         ArrayList<Integer> less = new ArrayList<>();
         ArrayList<Integer> sorted = new ArrayList<>();
@@ -33,10 +34,10 @@ public class project {
             //Conquer -- recursively sort smaller lists
             //pivot off first element -- naive quicksort
             if(greater_or_equal.size() >= 1) {
-                greater_or_equal = Quicksort(greater_or_equal, greater_or_equal.get(0));   
+                greater_or_equal = QuickSort(greater_or_equal, greater_or_equal.get(0));   
             }
             if(less.size() >= 1) {
-                less = Quicksort(less,less.get(0));
+                less = QuickSort(less,less.get(0));
             }
         }
 
@@ -46,10 +47,72 @@ public class project {
         sorted.addAll(greater_or_equal);
         return sorted;
     }
+    //O(n)            
+    private static ArrayList<Integer> Merge(ArrayList<Integer> left, ArrayList<Integer> right) {
+        ArrayList<Integer> merged = new ArrayList(); 
+        int vals_left = left.size()+right.size();
+        //Keep going until we are out of values to merge in
+        for(int i = vals_left; i>0; i--) {
+            //if one list is empty, just dump the rest in merged and return
+            if(left.size() == 0) {
+                merged.addAll(right);
+                break;
+            }
+            if(right.size() == 0) {
+                merged.addAll(left);
+                break;
+            }
+            //otherwise, add whichever first value is smaller to merged. O(1)
+            if(left.get(0) <= right.get(0)) {
+                merged.add(left.get(0));
+                left.remove(0);
+                continue;
+            }
+            else {
+                merged.add(right.get(0));
+                right.remove(0);
+            }
+        }
+        
+        return merged;   
+    }
+    
+    public static ArrayList<Integer> MergeSort(ArrayList<Integer> list) {
+        int half;
+        ArrayList<Integer> left;
+        ArrayList<Integer> right;
+        //divide
+        if(list.size() > 1) {
+             half = list.size()/2;
 
+             left = new ArrayList<>(list.subList(0, half));
+             right = new ArrayList<>(list.subList(half, list.size()));
+             
+             //subList() returns an emptylsit if start = end. This fixes that
+             if(half == list.size()) {
+                  right = new ArrayList<>();
+                  right.add(list.get(list.size()-1));
+             }
+             if(half == 0) {
+                 left = new ArrayList<>();
+                 right.add(list.get(0));
+             }
+             
+             //recurse
+             right =MergeSort(right);
+             left = MergeSort(left);
+        }
+        
+        else {
+            return list;
+        }
+        //conquer and combine
+        return Merge(left, right);
+    }
+    
     public static void main(String[] args) {
      
-        int list_size = 100;
+        int list_size = 10;
         int max = list_size*10;
         Random r = new Random();
         ArrayList<Integer> list = new ArrayList<>();
@@ -62,12 +125,18 @@ public class project {
         ArrayList<Integer> sorted;
         //get start time
         long startTime = System.nanoTime();
-        sorted = Quicksort(list, list.get(0));
+        sorted = QuickSort((ArrayList<Integer>)list.clone(), list.get(0));
         long endTime   = System.nanoTime();
-        long totalTime = endTime - startTime;
+        long totalTimeQS = endTime - startTime;
+        
+        startTime = System.nanoTime();
+        sorted = MergeSort((ArrayList<Integer>) list.clone());
+        endTime   = System.nanoTime();
+        long totalTimeMS = endTime - startTime;
         
         System.out.println(sorted);
-        System.out.println("Time to QuickSort = " + totalTime + "ns"); 
+        System.out.println("Time to QuickSort = " + totalTimeQS + " ns");
+        System.out.println("Time to MergeSort = " + totalTimeMS + " ns");
 
     }
 }
